cb7c4bd0ddf6f152caeafd11d82337cc889ed8b1
aam198 <aam198@psu.edu>
2023-02-02 14:35:04 -0500

    Configure Express server

--- index.js
+++ index.js
@@ -6,13 +6,29 @@ const path = require('path')
 const PORT = process.env.PORT || 3000
 
 
-// To use Routes 
-// app.get('/', (req, res) => {
-//   res.send('hello world');
-// })
+// Routes 
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/pages', express.static(path.join(__dirname, 'public')));
 
+// to route to styles dir
+app.use(express.static(path.join(__dirname, 'public', 'styles')));
+
+app.use(express.static(path.join(__dirname, 'public', 'src')));
+
+// Home
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/pages/index.html'));
+});
+
+// Accessibility 
+app.get('/acc', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/pages/acc.html'));
+});
+
+// 404
+app.use(function (req, res, next) {
+  res.status(404);
+  res.sendFile(path.join(__dirname + '/public/pages/missing.html'));
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
-
